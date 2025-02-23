c97b2ed25e6f0d17806c4c38cba45e50cbe475f1
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-02-22 21:34:48 -0300

    adicionar classe anônima calculadoraAnonima

--- src/main/java/com/alura/funcoeslambda/FuncoeslambdaApplication.java
+++ src/main/java/com/alura/funcoeslambda/FuncoeslambdaApplication.java
@@ -1,6 +1,7 @@
 package com.alura.funcoeslambda;
 
 import com.alura.funcoeslambda.modelos.Calculadora;
+import com.alura.funcoeslambda.services.OperacoesFuncional;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +16,15 @@ public class FuncoeslambdaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Calculadora calculadora = new Calculadora();
-
 		int resultadoMultiplicacao = calculadora.multiplicacao(2, 2);
 		System.out.println(resultadoMultiplicacao);
+
+		OperacoesFuncional calculadoraAnonima = new OperacoesFuncional() {
+			@Override
+			public int multiplicacao(int a, int b) {
+				return a * b;
+			}
+		};
+		System.out.println(calculadoraAnonima.multiplicacao(3, 3));
 	}
 }
